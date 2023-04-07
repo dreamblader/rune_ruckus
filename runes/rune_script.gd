2172e914aa962d618a39c6b01268b34dd60ddf44
Carlos Gabriel Monnazzi <carlos.monnazzi@datum.inf.br>
2023-04-07 19:32:14 -0300

    started chain algorithm (still need some fixes)

--- runes/rune_script.gd
+++ runes/rune_script.gd
@@ -22,6 +22,7 @@ onready var sprite: AnimatedSprite = $AnimatedSprite
 onready var detectors: Array = [ $DetectUp, $DetectRigth, $DetectDown, $DetectLeft ]
 
 signal explode
+signal touch_the_ground
 
 
 func _ready() -> void:
@@ -35,20 +36,45 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if visible && !is_on_floor():
-		move_and_slide(Vector2(0, gravity), Vector2(0,-1)) #slide is creating weird bugs
+		move_and_slide(Vector2(0, gravity), Vector2(0,-1))
 		position.x = column_pos
+	elif is_on_floor():
+		emit_signal("touch_the_ground")
 
 
 func check_edges() -> void:
 	var index = 0
+	if !is_on_floor():
+		yield(self, "touch_the_ground")
 	for d in detectors:
 		var detector = d as RayCast2D
-		#TODO force raycast need to wait gravity act first
 		detector.force_raycast_update()
 		var collider = detector.get_collider()
 		if collider != null && collider.get_class() == my_class:
 			prints(self.name,"--->", collider.name, "AT", index)
 		index += 1
+
+
+func link(rune:Rune, position:int) -> void:
+	edges.insert(position, rune) 
+	add_power(position, 1)
+	rune.chain_call(self, position, 1)
+
+
+func chain_call(caller:Rune, from:int, value:int):
+	var opposite_side = from+2 if from < 2 else from-2
+	var next_rune = edges[from] # TODO maybe next is unknown yet. Need to reformat. Maybe call detector again?
+	edges.insert(opposite_side, caller)
+	add_power(from, value)
+	if next_rune != null:
+		next_rune.chain_call(self, from, 1)
+
+
+func add_power(position:int, value:int) -> void:
+	if position%2 == 0:
+		v_power+=value
+	else:
+		h_power+=value
 
 
 func snap_position() -> void:
