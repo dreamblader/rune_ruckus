5840a7ba076e4bb2e0da12e4d1264934b64a979e
Dreamblader <carlosgabrielmaster@gmail.com>
2023-09-19 19:19:40 -0300

    added more settings stuff

--- options/settings_control.gd
+++ options/settings_control.gd
@@ -1,20 +1,66 @@
 extends Node
 
+var settings: Settings
+var PATH: String = "user://settings.cfg"
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+#KEYs
+var fullscreen_key:String = "fullscreen"
+var resolution_key:String = "resolution"
+var vsync_key:String = "vsync"
+var mute_key:String = "mute"
+var masterVolumeDb_key:String = "masterVolumeDb"
+var musicVolumeDb_key:String = "musicVolumeDb"
+var sfxVolumeDb_key:String = "sfxVolumeDb"
+var voiceVolumeDb_key:String = "voiceVolumeDb"
+var controlType_key:String = "controlType"
+var controllerInputs_key:String = "controllerInputs"
+var keyboardInputs_key:String = "keyboardInputs"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(InputMap.get_actions()) # get actions name
-	print(InputMap.get_action_list("drop"))
+	load_settings()
+	#print(InputMap.get_actions()) # get actions name
+	#print(InputMap.get_action_list("drop"))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func load_settings() -> void:
+	var config_file = ConfigFile.new()
+	if config_file.load(PATH) != OK:
+		settings = Settings.new()
+		set_config_settings(config_file)
+		config_file.save(PATH)
+	else:
+		prints(config_file, config_file.get_sections())
+		settings = get_config_settings(config_file)
+
+
+func get_config_settings(config:ConfigFile) -> Settings:
+	var result = Settings.new()
+	result.fullscreen = config.get_value(Settings.Section.keys()[Settings.Section.VIDEO], fullscreen_key)
+	# TODO
+	return result
+
+
+func set_config_settings(config:ConfigFile) -> void:
+	config.set_value(Settings.Section.keys()[Settings.Section.VIDEO], fullscreen_key, settings.fullscreen)
+	config.set_value(Settings.Section.keys()[Settings.Section.VIDEO], resolution_key, settings.resolution)
+	config.set_value(Settings.Section.keys()[Settings.Section.VIDEO], vsync_key, settings.vsync)
+	config.set_value(Settings.Section.keys()[Settings.Section.AUDIO], mute_key, settings.mute)
+	config.set_value(Settings.Section.keys()[Settings.Section.AUDIO], masterVolumeDb_key, settings.masterVolumeDb)
+	config.set_value(Settings.Section.keys()[Settings.Section.AUDIO], musicVolumeDb_key, settings.musicVolumeDb)
+	config.set_value(Settings.Section.keys()[Settings.Section.AUDIO], sfxVolumeDb_key, settings.sfxVolumeDb)
+	config.set_value(Settings.Section.keys()[Settings.Section.AUDIO], voiceVolumeDb_key, settings.voiceVolumeDb)
+	config.set_value(Settings.Section.keys()[Settings.Section.CONTROLS], controlType_key, settings.controlType)
+	config.set_value(Settings.Section.keys()[Settings.Section.CONTROLS], controllerInputs_key, settings.controllerInputs)
+	config.set_value(Settings.Section.keys()[Settings.Section.CONTROLS], keyboardInputs_key, settings.keyboardInputs)
+
+
+func save_settings() -> void:
+	var config_file = ConfigFile.new()
+	set_config_settings(config_file)
+	prints("DEBUG:", config_file.get_sections())
+	config_file.save(PATH)
+
 
 # CONTROL EXAMPLE
 #		for action in config.get_section_keys("input"):
--- options/settings_resource.gd
+++ options/settings_resource.gd
@@ -1,9 +1,12 @@
 extends Resource
+class_name Settings
+
+enum Section {VIDEO, AUDIO, CONTROLS}
 
 # VIDEO
-var fullscreen: bool = OS.is_window_fullscreen
-var resolution:Vector2 = OS.get_window_size
-var vsync: bool = OS.is_vsync_enabled
+var fullscreen: bool = OS.is_window_fullscreen() setget set_fullscreen
+var resolution:Vector2 = OS.get_window_size()
+var vsync: bool = OS.is_vsync_enabled()
 # AUDIO
 var mute: bool = false
 var masterVolumeDb: float = 0.0
@@ -14,3 +17,10 @@ var voiceVolumeDb: float = 0.0
 var controlType : int = 0 # 0 = Keyboard | 1 = Controller
 var controllerInputs: Dictionary = {}
 var keyboardInputs: Dictionary = {}
+
+
+func set_fullscreen(new_value):
+	fullscreen = new_value
+	OS.set_window_fullscreen(fullscreen)
+
+#TODO add other setters
