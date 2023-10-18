85099df5ea3ad4e0ef6e0c4aa93aeb4f5e86f94e
Dreamblader <carlosgabrielmaster@gmail.com>
2023-10-17 21:29:00 -0300

    added spell commands

--- ui/spell_block/spell_block_script.gd
+++ ui/spell_block/spell_block_script.gd
@@ -13,21 +13,34 @@ export (SpriteFrames) var lock
 export (Rune.COLOR) var my_symbol = -1 setget change_symbol
 export (LockType) var my_lock
 
-onready var symbol:TextureRect = $Label
+onready var symbol:Sprite = $Sprite
+onready var animation:AnimationPlayer = $AnimationPlayer
 
+var symbol_is_visible:bool = true
 
 func _ready() -> void:
 	get_lock()
 
 
 func unlock_block() -> void:
-	my_symbol = Rune.COLOR.NONE
-	change_symbol(my_symbol)
+	yeet_symbol()
+
+
+func cast_spell(valid_spell:bool) -> void:
+	if valid_spell:
+		consume_symbol()
+	else:
+		yeet_symbol()
 
 
 func change_symbol(new_symbol:int) -> void:
 	if my_symbol >= 0:
 		my_symbol = new_symbol
+		
+		if my_symbol != Rune.COLOR.NONE && symbol_is_visible:
+			disappear_symbol()
+			return
+		
 		match new_symbol:
 			Rune.COLOR.RED:
 				symbol.texture = red_symbol
@@ -37,6 +50,10 @@ func change_symbol(new_symbol:int) -> void:
 				symbol.texture = yellow_symbol
 			_:
 				symbol.texture = null
+		
+		appear_symbol()
+	else:
+		shake_lock()
 
 
 func get_lock() -> void:
@@ -52,15 +69,31 @@ func get_lock() -> void:
 
 
 func yeet_symbol() -> void:
-	#TODO: FUTURE - add tween to explode the symbol/lock out of this panel (when spell is wrong/ unlock panel)
-	pass
+	my_symbol = Rune.COLOR.NONE
+	animation.play("yeet")
+
+
+func disappear_symbol() -> void:
+	symbol_is_visible = false
+	animation.play("disappear")
+
+
+func appear_symbol() -> void:
+	symbol_is_visible = true
+	animation.play("appear")
 
 
 func consume_symbol() -> void:
-	#TODO: FUTURE - add tween to fade symbol (when spell is OK)
-	pass
+	my_symbol = Rune.COLOR.NONE
+	animation.play("consume")
 
 
 func shake_lock() -> void:
-	#TODO: check if locked and then shake it
-	pass
+	animation.play("shake")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"yeet", "consume", "disappear":
+			symbol.rotation_degrees = 0
+			change_symbol(my_symbol)
