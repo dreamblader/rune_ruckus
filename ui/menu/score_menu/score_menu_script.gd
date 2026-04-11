bfc50ca3b94011a6175fba92d81068efdf441d7b
Dreamblader <carlosgabrielmaster@gmail.com>
2026-04-11 01:31:30 -0300

    godot 4.6 + score menu scene WIP

--- ui/menu/score_menu/score_menu_script.gd
+++ ui/menu/score_menu/score_menu_script.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+@onready var animation = $AnimationPlayer
 var controller_mode = false
 
+
 func start_record(rank:int, score:float, spells_count:Dictionary, game_time:float) -> void:
+	animation.play("enter_end_mode")
 	self.visible = true
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
